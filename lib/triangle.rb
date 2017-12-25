ca2913748b06664f37e3a025d88b6c172d1baf11
Brandon Leeds <brandonleeds@gmail.com>
2017-12-25 22:48:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,7 @@ class Triangle
     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
       raise TriangleError
     end
+    if (@side_1 + @side_2 < @side_3) || (@side_3 + @side_1 < @side_2) || (@side_2 + @side_3 < @side_1)
     return :equilateral if [@side_1, @side_2, @side_3].uniq.count <= 1
     return :isosceles if [@side_1, @side_2, @side_3].uniq.count <= 2
     return :scalene if [@side_1, @side_2, @side_3].uniq.count == 3
